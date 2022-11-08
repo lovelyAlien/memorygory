563e48f68e394776becd84aca6473c19a428e04b
unknown <jaeseung_choun@tmax.co.kr>
2022-11-08 11:00:46 +0900

    first commit

---
+++ src/main/java/com/tmax/memorygory/MemorygoryApplication.java
@@ -0,0 +1,13 @@
+package com.tmax.memorygory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemorygoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MemorygoryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tmax/memorygory/MemorygoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tmax.memorygory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemorygoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
